fb121c7e20d9dd0c095fed2b4665ca54494b483d
unknown <kylelarsen12@gmail.com>
2022-06-08 18:20:14 -0400

    Rough sketch of board with a better starting place

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Function to draw odd ranks
 /*
+//Function to draw odd ranks
 function drawOddRank() {
   for (let i = 1; i <= 8; i++) {
     let square = document.createElement("div");
@@ -38,3 +38,32 @@ drawOddRank();
 drawEvenRank();
 drawOddRank();
 */
+
+//define ranks and files
+let rank = [1, 2, 3, 4, 5, 6, 7, 8];
+let file = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+//function to draw chessboard
+function createBoard() {
+  let chessboard = [];
+  let board = document.getElementById("gameBoard");
+  let squareName;
+
+  //nested for loop to go through ranks and files
+  for (let i = 0; i < rank.length; i++) {
+    for (let j = 0; j < file.length; j++) {
+      chessboard.push(`${file[i]}${rank[j]}`);
+      squareName = file[i] + rank[j];
+    }
+  }
+
+  //for loop to create div and name each square
+  for (let i = 0; i < chessboard.length; i++) {
+    let newTile = document.createElement("div");
+    newTile.id = chessboard[i];
+    newTile.textContent = chessboard[i];
+    board.append(newTile);
+  }
+}
+
+createBoard();
